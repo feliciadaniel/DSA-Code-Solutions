94ca60a820f56abd005f9cf4ebbbbb0f591ee862
feliciadaniel <100063610+feliciadaniel@users.noreply.github.com>
2023-08-03 09:49:06 +0530

    Create Ninja And The Sorted Check.java

---
+++ Ninja And The Sorted Check.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static int isSorted(int n, int []a) {
+        // Write your code here.
+        int flag=1;
+        for(int i=0;i<n-1;i++){
+            if(a[i]>a[i+1]){
+            flag=0;
+            break;
+            }
+            
+        }
+        return flag;
+    }
+}
