093ab467485f0d759de660bba101bc231700893a
feliciadaniel <100063610+feliciadaniel@users.noreply.github.com>
2024-03-04 11:22:17 +0530

    Time: 4 ms (55.88%), Space: 53.6 MB (6.53%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        if (n==1){
+            return nums[0];
+        }
+        return nums[(n/2)];
+    }
+}
