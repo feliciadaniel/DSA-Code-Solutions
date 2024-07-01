ee7a7d906f3dbb1e6c0ec9ddb6e3a2b6d305e83a
feliciadaniel <100063610+feliciadaniel@users.noreply.github.com>
2024-07-01 21:00:01 +0530

    Time: 0 ms (100%), Space: 42 MB (73.84%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count= 0;
+        for(int x: arr){
+            if(x%2!=0){
+                count++;
+            }
+            else{
+                count=0;
+            }
+            if(count==3){
+                return true;
+            }
+        }
+        return false;
+    }
+}
